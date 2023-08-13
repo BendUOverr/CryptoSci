87350dd9e973f9cc63fed12d1d97737125da3786
BendUOverr <luka.buadzee@gmail.com>
2023-08-13 23:43:03 +0200

    ClearingFiles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./routes/Home";
 import Signup from "./routes/Signup";
-import { Account } from "./routes/Account";
 import { CoinPage } from "./routes/CoinPage";
 import axios from 'axios'
 import Footer from "./components/Footer";
@@ -26,7 +25,6 @@ function App() {
       <Route path='/' element={<Home coins={coins} />} />
       <Route path='/signin' element={<Signin />} />
       <Route path='/signup' element={<Signup/>} />
-      <Route path='/account' element={<Account />} />
       <Route path='/coin/:coinId' element={<CoinPage />}>
         <Route path='coinId' />
       </Route>
